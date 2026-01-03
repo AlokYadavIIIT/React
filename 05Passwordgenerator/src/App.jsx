bb0bdf194700bbf4c193465b458a13c952d5ab51
Alok Yadav <yalok2372@gmail.com>
2026-01-04 00:42:05 +0530

    Project 5:Password Generator

--- 05Passwordgenerator/src/App.jsx
+++ 05Passwordgenerator/src/App.jsx
@@ -37,10 +37,12 @@ function App() {
   useEffect(() => {
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
+  
   return (
     
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
       <h1 className='text-white text-center my-3'>Password generator</h1>
+    
     <div className=" flex shadow rounded-lg overflow-hidden mb-4">
         <input
             type="text"
@@ -53,9 +55,9 @@ function App() {
         <button
         onClick={copyPasswordToClipboard}
         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
-        >copy</button>
-        
+        >copy</button>    
     </div>
+
     <div className='flex text-sm gap-x-2'>
       <div className='flex items-center gap-x-1'>
         <input 
@@ -63,11 +65,12 @@ function App() {
         min={6}
         max={100}
         value={length}
-         className='cursor-pointer'
-         onChange={(e) => {setLength(e.target.value)}}
+        className='cursor-pointer'
+        onChange={(e) => {setLength(e.target.value)}}
           />
-          <label>Length: {length}</label>
+        <label>Length: {length}</label>
       </div>
+
       <div className="flex items-center gap-x-1">
       <input
           type="checkbox"
@@ -79,6 +82,7 @@ function App() {
       />
       <label htmlFor="numberInput">Numbers</label>
       </div>
+
       <div className="flex items-center gap-x-1">
           <input
               type="checkbox"
@@ -90,7 +94,9 @@ function App() {
           />
           <label htmlFor="characterInput">Characters</label>
       </div>
+
     </div>
+    
 </div>
     
   )
