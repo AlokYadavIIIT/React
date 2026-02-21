d56930c3ed93ea8f316995a2bef1ca97bfaeefb5
Alok Yadav <yalok2372@gmail.com>
2026-02-21 18:00:51 +0530

    Project 9:Theme Switcher

--- 09themeswitcher/src/contexts/theme.js
+++ 09themeswitcher/src/contexts/theme.js
@@ -10,6 +10,6 @@ export const ThemeContext = createContext({
 
 export const ThemeProvider = ThemeContext.Provider;
 
-export default function useTheme(){
+export default function useTheme(){//custom hook
     return useContext(ThemeContext);
 }
